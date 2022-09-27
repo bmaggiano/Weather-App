acbb277f90e5b4106d732eaea16605977a482185
bmaggiano <brandon.maggiano@gmail.com>
2022-09-26 18:33:16 -0700

    completed fetch api js for current day, added running list of searched cities, added header to index.html

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,54 @@
-// let weatherUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
-let apiKey = "f5f2f5a01947a3884ab3b3064c13c0e2"
-let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=33.4484367&lon=-112.074141&appid=f5f2f5a01947a3884ab3b3064c13c0e2&units=imperial`
-let exmaple = "http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=1&appid=f5f2f5a01947a3884ab3b3064c13c0e2"
+const citySpan = document.getElementById('city');
+const tempSpan = document.getElementById('temp');
+const windSpan = document.getElementById('wind');
+const humiditySpan = document.getElementById('humidity');
+const searchedEl = document.getElementById('searched-cities');
+const button = document.getElementById('search');
+const cityForm = document.getElementById('city-name');
+const dateSpan = document.getElementById('date')
+// const cityInput = cityForm.value
 
-fetch(weatherUrl)
+
+
+// let apiKey = "f5f2f5a01947a3884ab3b3064c13c0e2"
+// let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityForm.value}&appid=f5f2f5a01947a3884ab3b3064c13c0e2&units=imperial`
+
+
+function weatherAPI() {
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityForm.value}&appid=f5f2f5a01947a3884ab3b3064c13c0e2&units=imperial`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data)
-        console.log(data.city.name)
-        console.log(`${data.list[4].main.temp} F`)
-        console.log(moment.unix(data.list[4].dt).format('MMMM Do YYYY'))
-        console.log(`${data.list[12].main.temp} F`)
-        console.log(moment.unix(data.list[12].dt).format('MMMM Do YYYY'))
-        console.log(`${data.list[20].main.temp} F`)
-        console.log(moment.unix(data.list[20].dt).format('MMMM Do YYYY'))
-        console.log(`${data.list[28].main.temp} F`)
-        console.log(moment.unix(data.list[28].dt).format('MMMM Do YYYY'))
-        console.log(`${data.list[36].main.temp} F`)
-        console.log(moment.unix(data.list[36].dt).format('MMMM Do YYYY'))
+        citySpan.textContent = (data.city.name)
+        tempSpan.textContent = (`${data.list[6].main.temp} F`)
+        windSpan.textContent = (`${data.list[6].wind.speed} MPH`)
+        humiditySpan.textContent = (`${data.list[6].main.humidity}%`)
+        dateSpan.textContent = (moment.unix(data.list[6].dt).format('MMMM Do YYYY'))
+        
+
+        const searchedList = document.createElement('p')
+        searchedList.textContent = (`You've searched ${cityForm.value}`)
+
+        searchedEl.appendChild(searchedList)
+
+
+
+
+
+        console.log(`${data.list[6].main.temp} F`)
+        console.log(`${data.list[14].main.temp} F`)
+        console.log(moment.unix(data.list[14].dt).format('MMMM Do YYYY'))
+        console.log(`${data.list[22].main.temp} F`)
+        console.log(moment.unix(data.list[22].dt).format('MMMM Do YYYY'))
+        console.log(`${data.list[30].main.temp} F`)
+        console.log(moment.unix(data.list[30].dt).format('MMMM Do YYYY'))
+        console.log(`${data.list[38].main.temp} F`)
+        console.log(moment.unix(data.list[38].dt).format('MMMM Do YYYY'))
+    })
+}
+    button.addEventListener('click', function () {
+        console.log(cityForm.value)
+        weatherAPI()
     })
